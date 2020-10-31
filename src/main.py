00da3ae792c4e91a030b5ebf4d965f70dcabf6ef
Paulo <paulozamora2000@gmail.com>
2020-10-30 19:55:44 -0600

    Función para extraer prerequisitos de materias. Quite el destructor.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,3 @@
 from modules.lectorExcel import Lector
+
+l = Lector('../testData/datosPrincipales.xlsx', '../testData/datosAdicionales.xlsx')
--- src/modules/lectorExcel.py
+++ src/modules/lectorExcel.py
@@ -1,8 +1,29 @@
+from openpyxl import load_workbook
+
 class Lector:
+    _wbPrincipal = None
+    _wbAdicional = None
     def __init__(self, archivoPrincipal, archivoAdicionales):
         self._wbPrincipal = load_workbook(filename=archivoPrincipal)
         self._wbAdicional = load_workbook(filename=archivoAdicionales)
+        self._prerequisitos = self.extraerPrerequisitos()
 
-    def __del__(self):
-        self._wbPrincipal.close()
-        self._wbAdicional.close()
+    def extraerPrerequisitos(self):
+        prerequisitos = {}
+        try:
+            ws = self._wbAdicional['Prerequisitos']
+        except:
+            print('Hoja con prerequisitos de materias no encontrado')
+            sys.exit(1)
+
+        for celda in tuple(ws.columns)[0]:
+            if celda.row != 1:
+                programa = ws.cell(column = 1, row = celda.row).value
+                materia = ws.cell(column = 2, row = celda.row).value
+                prerequisito = ws.cell(column = 3, row = celda.row).value
+
+                if not programa in prerequisitos:
+                    prerequisitos[programa] = {}
+
+                prerequisitos[programa][materia.upper()] = prerequisito
+        return prerequisitos
