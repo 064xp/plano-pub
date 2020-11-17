e68997f7763964f9006a1157df48ce579012fe87
eojedagalvan <eduardo.ojeda@edu.uag.mx>
2020-11-17 13:48:18 -0600

    Función Obtener cuatri

--- src/modules/asignador.py
+++ src/modules/asignador.py
@@ -10,10 +10,38 @@ class Asignador:
             for materia in materias:
                 materia.alumnos.append(alumno.registro)
 
-    def promediarCuatri(self, materiasPendientes):
-        sumaCuatri = 0
-        for materia in materiasPendientes:
-            sumaCuatri+= materia.cuatri
-        promedio = int(sumaCuatri/len(materiasPendientes))
+    # def promediarCuatri(self, materiasPendientes):
+    #     sumaCuatri = 0
+    #     for materia in materiasPendientes:
+    #         sumaCuatri+= materia.cuatri
+    #     promedio = int(sumaCuatri/len(materiasPendientes))
+    #
+    #     return promedio
 
-        return promedio
+def calcularCuatri(self,materiasPendientes):
+    cuatriMayor = 0
+    cantidadMaterias = 0
+    cuatri = 0
+    cantidad = []
+    for materia in materiasPendientes:
+        if cuatriMayor < materia.cuatri:
+            cuatriMayor = materia.cuatri
+        else:
+            continue
+
+    for i in range(cuatriMayor):
+        cantidad.append(0)
+
+    for materia in materiasPendientes:
+        if cantidad[materia.cuatri-1] >= 1:
+            cantidad[materia.cuatri-1]+=1
+
+        else:
+            cantidad[materia.cuatri-1] = 1
+
+    for i in range(len(cantidad)):
+       if cantidadMaterias < cantidad[i]:
+           cantidadMaterias = cantidad[i]
+           cuatri = i
+
+    return cuatri+1
