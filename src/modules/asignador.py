636d31b36d948d198307f479f4b567d50f9cb277
Paulo <paulozamora2000@gmail.com>
2020-11-17 12:58:19 -0600

    Función para crear grupos después de la asignación.

--- src/modules/asignador.py
+++ src/modules/asignador.py
@@ -1,7 +1,12 @@
+from modules.definiciones.Grupo import Grupo
+
 class Asignador:
-    def __init__(self, alumnos, programas):
+    def __init__(self, alumnos, programas, materias, minAlumnos = 10, maxAlumnos=26):
         self.alumnos = alumnos
         self.programas = programas
+        self.materias = materias
+        self.minAlumnos = minAlumnos
+        self.maxAlumnos = maxAlumnos
 
     def asignarMaterias(self):
         for alumno in self.alumnos:
@@ -11,7 +16,7 @@ class Asignador:
             materias = alumno.materiasPendientes[0:materiasPorCuatri]
 
             for materia in materias:
-                materia.alumnos.append(alumno.registro)
+                materia.alumnos.append(alumno)
 
     def promediarCuatri(self, materiasPendientes):
         sumaCuatri = 0
@@ -19,3 +24,16 @@ class Asignador:
             sumaCuatri+= materia.cuatri
         promedio = int(sumaCuatri/len(materiasPendientes))
         return promedio
+
+    def crearGrupos(self):
+        for llaveMateria in self.materias.keys():
+            grupos = []
+            materia = self.materias[llaveMateria]
+            for alumno in materia.alumnos:
+                alumno.materiasPendientes.remove(materia)
+            while len(materia.alumnos) >= self.minAlumnos:
+                alumnosGrupo = materia.alumnos[:self.maxAlumnos]
+                materia.alumnos = materia.alumnos[self.maxAlumnos:]
+                grupos.append(Grupo(alumnosGrupo, materia.nombre))
+
+            materia.grupos = grupos
---
+++ src/modules/definiciones/Grupo.py
@@ -0,0 +1,4 @@
+class Grupo:
+    def __init__(self, alumnos, materia):
+        self.alumnos = alumnos
+        self.materia = materia
